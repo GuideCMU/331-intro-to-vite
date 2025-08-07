231d768340fa1a09d6c067e16f25403ef05c2e0d
Saengchailnwza <grgamerkung@gmail.com>
2025-08-08 01:43:40 +0700

    13.7 fix import

--- lab2compo/src/main.ts
+++ lab2compo/src/main.ts
@@ -1,15 +1,14 @@
 import './assets/style.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
+import { inject } from "@vercel/analytics"
 
 const app = createApp(App)
 
-import { inject } from '@vercel/analytics'
+
 inject ()
 app.use(createPinia())
 app.use(router)
